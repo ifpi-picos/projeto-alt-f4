03f24118736ede65023accd9e126df85b2979560
Jailson Jhonatas <mrjk044@gmail.com>
2020-10-29 18:58:37 -0300

    active-close game-content

--- src/scripts.js
+++ src/scripts.js
@@ -7,10 +7,13 @@ for (let card of cards) {
         modalOverlay.classList.add('active')
         modalOverlay.querySelector('iframe').src = `https://www.youtube.com/embed/${videoId}`
 
+        modalOverlay.querySelector(`#${videoId}`).classList.add('active2');
+
     })
 }
 
 document.querySelector(".close-modal").addEventListener("click", function() {
     modalOverlay.classList.remove('active')
     modalOverlay.querySelector('iframe').src = ""
-})
+    modalOverlay.querySelector('.game-content').classList.remove('active2')
+});
