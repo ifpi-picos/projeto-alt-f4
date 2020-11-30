9899595b6507faea12753005070a273b4eefcddb
Jailson Jhonatas <mrjk044@gmail.com>
2020-11-30 20:25:44 -0300

    style game vue

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -29,4 +29,172 @@
 </template>
 
 <style lang="scoped">
+  @font-face {
+    font-family: century-gothic;
+    src: url("../font/century-gothic.ttf");
+  }
+
+  * {
+    padding: 0;
+    margin: 0;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+
+  a {
+    color: #fff;
+    text-decoration: none;
+  }
+
+  html,
+  body {
+    width: 100%;
+    height: 100%;
+    line-height: 1.5;
+    /* background: linear-gradient(to bottom, #90C0DC 50%, #fff) no-repeat; */
+  }
+
+  body,
+  input,
+  textarea {
+    font-family: century-gothic;
+  }
+
+  .container {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 10vh 70vh 20vh;
+    grid-template-areas: "header" "main" "footer";
+  }
+
+  header {
+    grid-area: header;
+  }
+
+  main {
+    grid-area: main;
+  }
+
+  footer {
+    grid-area: footer;
+  }
+
+  /* ---- MAIN ---- */
+
+  .title .subtitle {
+    font-weight: bold;
+  }
+
+  .title {
+    font-size: 1.8em;
+  }
+
+  .subtitle {
+    font-size: 1.2em;
+  }
+
+  .principal {
+    width: 85vw;
+    height: 600px;
+    margin: 30px auto 80px;
+  }
+
+  .card {
+    /*  */
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr;
+    grid-gap: 20px;
+    position: relative;
+  }
+
+  .cards img {
+    padding: 20px 20px;
+    width: 100%;
+    max-width: 100%;
+    height: auto;
+    border-radius: 8%;
+  }
+
+  .card-jogos {
+    position: relative;
+    display: flex;
+  }
+
+  .card-jogos button {
+    position: absolute;
+    bottom: 250px;
+    left: 700px;
+  }
+
+  /* ---- GAME DESCRIPTION ---- */
+
+  .desc-game {
+    height: 100vh;
+    text-align: center;
+    white-space: wrap;
+  }
+
+  .game-content {
+    text-align: justify;
+  }
+
+  .game-content > ol {
+    list-style: none;
+    margin: 10px;
+  }
+
+  /* ---- VIDEO ---- */
+
+  iframe {
+    margin-top: 30px;
+  }
+
+  .desc-game p {
+    font-size: 1em;
+    word-wrap: break-word;
+    word-break: keep-all;
+  }
+
+  .desc-game ol li {
+    margin: 20px;
+    list-style: decimal;
+    list-style-position: inside;
+  }
+
+  /* ---- DOWNLOAD GAME ---- */
+
+  .download-game img {
+    width: 50px;
+    height: 50px;
+    margin: auto;
+    display: block;
+  }
+
+  .download-game a {
+    padding: 5px;
+    font-weight: bold;
+    color: #4e97c3;
+  }
+
+  /* ---- GAME CONTACT ---- */
+
+  .game-contato {
+    margin-bottom: 100px;
+  }
+
+  .game-contato p {
+    margin: 50px 0 30px 0;
+    font-weight: bold;
+  }
+
+  .game-contato a {
+    padding: 5px;
+    font-weight: bold;
+    padding: 10px;
+    background-color: #4e97c3;
+    border-radius: 10px;
+    margin-top: 50px;
+  }
 </style>
