5728ccd3ed33a74a474ed2348c522cdd49adbada
Jailson Jhonatas <mrjk044@gmail.com>
2020-11-30 12:27:40 -0300

    Vue Games

--- src/router/index.js
+++ src/router/index.js
@@ -4,31 +4,46 @@ import Padrao from '../template/Padrao.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'Template',
     component: Padrao,
     redirect: '/home',
-    children: [
-      {
-        path: '/home',
-        name: 'Home',
-        component: () => import('../views/Home.vue')
-      },
-      {
-        path: '/about',
-        name: 'About',
-        component: () => import('../views/About.vue')
-      }
+    children: [{
+            path: '/home',
+            name: 'Home',
+            component: () =>
+                import ('../views/Home.vue')
+        },
+        {
+            path: '/about',
+            name: 'About',
+            component: () =>
+                import ('../views/About.vue')
+        }, {
+            path: '/game',
+            name: 'Game',
+            component: () =>
+                import ('../views/Game.vue')
+        },
+        {
+            path: './games/avelar',
+            name: 'Avelar',
+            component: () =>
+                import ('../views/games/avelar.vue')
+        }, {
+            path: './games/meme',
+            name: 'Meme',
+            component: () =>
+                import ('../views/games/meme.vue')
+        }
     ]
-  }
-]
+}]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
