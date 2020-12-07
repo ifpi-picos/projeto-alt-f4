bec679c1191d62eb2aca99bf5d8fd70bad179af5
Janderson <51001932+droid-janderson@users.noreply.github.com>
2020-12-07 12:34:37 -0300

    Deploy no firebase

---
+++ deploy/public/precache-manifest.482924dc1621e9c9ce453a74029bb115.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a27a4163254fc8fce870c8cc3a3f94f",
+    "url": "/404.html"
+  },
+  {
+    "revision": "ce5a770e654446fe18fe",
+    "url": "/css/app.544be617.css"
+  },
+  {
+    "revision": "dee7c352e0730eecfee0",
+    "url": "/css/chunk-096b59d8.4b7d01a8.css"
+  },
+  {
+    "revision": "0c85a56095f577093e0d",
+    "url": "/css/chunk-1998c644.a072d7ca.css"
+  },
+  {
+    "revision": "6405b59e5b73693151b4",
+    "url": "/css/chunk-f1b36006.8ce76294.css"
+  },
+  {
+    "revision": "d327df1966905ab8e2e99dc7c76f7c7b",
+    "url": "/img/640x790/AVELAR.png"
+  },
+  {
+    "revision": "a80190fcc5ba4a30930e37132a06b750",
+    "url": "/img/640x790/Lendas RPG.png"
+  },
+  {
+    "revision": "818ed81fb5319990ae30969e072e9269",
+    "url": "/img/640x790/Push Trash.png"
+  },
+  {
+    "revision": "6d609e36b089d726916b950ac285d8d5",
+    "url": "/img/640x790/memecardarena.png"
+  },
+  {
+    "revision": "e98739c4bf7faf81d1d6a7b15d6fda47",
+    "url": "/img/640x790/reborn.png"
+  },
+  {
+    "revision": "98f1e5edf08620fb74b912c5374b57bb",
+    "url": "/img/900X900/AVELAR.png"
+  },
+  {
+    "revision": "67b360b3d41f2aeb47dc1adb8e87d1cf",
+    "url": "/img/900X900/LENDAS RPG.png"
+  },
+  {
+    "revision": "2d3e7ca75342a6dc353c8ffdf5984e17",
+    "url": "/img/900X900/MEMEMCARDARENA.png"
+  },
+  {
+    "revision": "17ce6828b1990bea1fab094dc71eb09a",
+    "url": "/img/900X900/PUSHTRASH.png"
+  },
+  {
+    "revision": "e1495b1951068465ca12d34b74c160f3",
+    "url": "/img/900X900/REBORN.png"
+  },
+  {
+    "revision": "e5f937e005acd3f2445c3df981d05a8d",
+    "url": "/img/full hd/MEMEMCARD.png"
+  },
+  {
+    "revision": "ca5b02c56e87624f05062328ddd60e61",
+    "url": "/img/full hd/REBORN.png"
+  },
+  {
+    "revision": "b72ec0c4c7d1c7f58268b870eb3e8062",
+    "url": "/img/full hd/avelar.png"
+  },
+  {
+    "revision": "08e4e3a3257ca9bb1321ddd3859c62df",
+    "url": "/img/full hd/lendasrpg.png"
+  },
+  {
+    "revision": "91dc05e3219193f1a7ca2bb6409ed500",
+    "url": "/img/full hd/pushtrahs.png"
+  },
+  {
+    "revision": "a61a3ff3fc403047ed65308267a75f50",
+    "url": "/img/logo/RD.png"
+  },
+  {
+    "revision": "c88472338d5a3fb4971a5f50104cf2c5",
+    "url": "/img/logo/logo.png"
+  },
+  {
+    "revision": "964553c6a9092ed57fbd026ab371e04c",
+    "url": "/img/logo/logo.svg"
+  },
+  {
+    "revision": "b9b0f77cf6bf9f69137c6eff4bcad24b",
+    "url": "/img/logo/logo1.png"
+  },
+  {
+    "revision": "dae59c2d3b71b4eb8f960a60ea0f8522",
+    "url": "/img/logo/logo2.png"
+  },
+  {
+    "revision": "f2a2702cceb8d6a6e0ee0dc35e7a5c7e",
+    "url": "/img/logo/makingDreams.png"
+  },
+  {
+    "revision": "81edde9ecce4ab69aaa46eddde7adcf5",
+    "url": "/img/svg/github.svg"
+  },
+  {
+    "revision": "d4f9ec6201ee03bcf2e0676f1ab97aed",
+    "url": "/img/svg/insta.svg"
+  },
+  {
+    "revision": "ce23106b54eaa776e02c79c1afb1bdaa",
+    "url": "/img/svg/twitter.svg"
+  },
+  {
+    "revision": "2795c7063043450eb294a76acc4439d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce5a770e654446fe18fe",
+    "url": "/js/app.0ea7ac49.js"
+  },
+  {
+    "revision": "dee7c352e0730eecfee0",
+    "url": "/js/chunk-096b59d8.942cce42.js"
+  },
+  {
+    "revision": "0c85a56095f577093e0d",
+    "url": "/js/chunk-1998c644.ad700da9.js"
+  },
+  {
+    "revision": "ee55e4c2f8b2d9878eb3",
+    "url": "/js/chunk-2d0e1f26.182ab6cb.js"
+  },
+  {
+    "revision": "f24c55d812f5aaf6fca0",
+    "url": "/js/chunk-2d20811b.acbbc2fc.js"
+  },
+  {
+    "revision": "df8dde8b657b66fcff54",
+    "url": "/js/chunk-2d21a3d2.bbfc0c2e.js"
+  },
+  {
+    "revision": "6405b59e5b73693151b4",
+    "url": "/js/chunk-f1b36006.8cfa8c9a.js"
+  },
+  {
+    "revision": "da597d3247b1294f0bb4",
+    "url": "/js/chunk-vendors.5600fd92.js"
+  },
+  {
+    "revision": "5c9a65bce80d07735a76dcb49af5c00e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
