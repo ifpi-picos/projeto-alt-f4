a32faff34a590a7a5c9e0cacc285c00ee839b92d
Lukas00-gif <lucassky08@gmail.com>
2020-12-06 22:41:00 -0300

    correcoes nas paginas de sobre e contato

--- src/views/Contato.vue
+++ src/views/Contato.vue
@@ -1,14 +1,69 @@
 <template>
   <div class="conteiner">
-    <div class="row">
-      <div class="div.col-lg-8 col-md-10 mx-10">
-        <p>
-          Quer entrar em contato conosco? Preencha o formulário abaixo para me
-          enviar uma mensagem e vamos tentar responder para você dentro de 24
-          horas!
-        </p>
-      </div>
-    </div>
+    <p class="h5 col-md-6 mr-auto ml-auto" id="espaco">
+      Quer entrar em contato conosco? Preencha o formulário abaixo para me
+      enviar uma mensagem e vamos tentar responder para você dentro de 24 horas!
+    </p>
+    <!-- 
+        nome
+        email
+        numero de celular
+        texto area -->
+
+    <b-card class="col-md-6 ml-auto mr-auto bg-info">
+      <form>
+        <p class="h2 text-center mb-4">Contate-nos</p>
+
+        <!-- Fala o nome -->
+        <label for="nome" class="grey-text">Nome</label>
+        <input
+          type="text"
+          id="nome"
+          class="form-control"
+          placeholder="Digite Seu Nome"
+        />
+
+        <br />
+
+        <!-- fala o email -->
+        <!-- colocar o required depois no email -->
+        <label for="email" class="grey-text">E-mail</label>
+        <input
+          type="email"
+          id="email"
+          class="form-control"
+          placeholder="Digite seu Email"
+        />
+
+        <br />
+
+        <!-- telefone -->
+        <label for="telefone" class="grey-text">Telefone</label>
+        <input
+          type="number"
+          id="telefone"
+          class="form-control"
+          placeholder="Digite seu telefone"
+        />
+
+        <br />
+
+        <!-- deixe a mensagem -->
+        <label for="mensagem" class="grey-text">Mensagem</label>
+        <textarea
+          type="text"
+          id="mensagem"
+          class="form-control"
+          rows="3"
+          placeholder="digite sua mensagem"
+        ></textarea>
+
+        <div class="text-center mt-4">
+          <!-- <button class="btn btn-outline-warning"  type="submit">Enviar</button> -->
+          <button class="btn btn bg-success">Enviar</button>
+        </div>
+      </form>
+    </b-card>
   </div>
 </template>
 
@@ -16,5 +71,14 @@
 export default {};
 </script>
 
-<style>
+<style scoped>
+  .conteiner
+  {
+    padding: 20px 0px 35px;
+  }
+
+  #espaco
+  {
+    padding: 20px;
+  }
 </style>
--- src/views/Sobre.vue
+++ src/views/Sobre.vue
@@ -1,52 +1,37 @@
 <template>
   <div class="conteiner">
-      <div class="row">
-          <div class = 'div.col-lg-8 col-md-10 mx-10'>
-              <p>
-                "
-                   A  "
-                   <b>radical dreamers</b>
-                   "é uma empresa de desenvolvimento de jogos e de soluções 
-                   tecnologicas criativas.
-                                "
-              </p>
+    <div class="row">
+        <!-- faz com que tudo centralizado mr-auto ml auto -->
+       <div class="div.col-lg-8 col-md-10 mx-3 mr-auto ml-auto"> 
+        <p class="h5 col-md-8 mr-auto ml-auto">
+          A <b>radical dreamers</b>
+          é uma empresa de desenvolvimento de jogos e de soluções tecnologicas
+          criativas
+        </p>
 
-              <p>
-                  "
-                         Nossa "
-                    <b>Visão</b>
-                    "é: ser uma empresa de economia criativa reconhecida por fazer a
-                    diferença no arranjo produtivo local e na vida das pessoas.
-                                 "
-              </p>
+        <p class="h5 col-md-8 mr-auto ml-auto">
+          Nossa <b>Visão</b>
+          é: ser uma empresa de economia criativa reconhecida por fazer a
+          diferença no arranjo produtivo local e na vida das pessoas
+        </p>
 
-              <p>
-                "
-                    Nossa"
-                    <b>Missão</b>
-
-                    "é: criar uma cultura gamer atraves de produtos de qualidade 
-                    reconhecida, gerando valor, renda e oportunidades
-                                 "
-              </p>
-          </div>
+        <p class="h5 col-md-8 mr-auto ml-auto">
+          Nossa <b>Missão</b>
+          é: criar uma cultura gamer atraves de produtos de qualidade
+          reconhecida, gerando valor, renda e oportunidades
+        </p>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
-
-.conteiner{
-    /* height: 90%;
-    width: 90%; */
-
-    padding: 175px;
+.conteiner {
+  padding: 154px;
+  /* padding: 30px; */
 }
-
 </style>
