e63f7149aa278b4c01ad1494d450aad75999b0fe
Lukas00-gif <lucassky08@gmail.com>
2020-12-12 14:10:26 -0300

    formulario mudou e outras alteracoes

--- src/views/Sobre.vue
+++ src/views/Sobre.vue
@@ -3,19 +3,19 @@
     <div class="row">
         <!-- faz com que tudo centralizado mr-auto ml auto -->
        <div class="div.col-lg-8 col-md-10 mx-3 mr-auto ml-auto"> 
-        <p class="h5 col-md-8 mr-auto ml-auto">
+        <p class="h5 col-md-8 mr-auto ml-auto text-justify">
           A <b>radical dreamers</b>
           é uma empresa de desenvolvimento de jogos e de soluções tecnologicas
           criativas
         </p>
 
-        <p class="h5 col-md-8 mr-auto ml-auto">
+        <p class="h5 col-md-8 mr-auto ml-auto text-justify" id="espaco">
           Nossa <b>Visão</b>
           é: ser uma empresa de economia criativa reconhecida por fazer a
           diferença no arranjo produtivo local e na vida das pessoas
         </p>
 
-        <p class="h5 col-md-8 mr-auto ml-auto">
+        <p class="h5 col-md-8 mr-auto ml-auto text-justify">
           Nossa <b>Missão</b>
           é: criar uma cultura gamer atraves de produtos de qualidade
           reconhecida, gerando valor, renda e oportunidades
@@ -31,7 +31,10 @@ export default {};
 
 <style scoped>
 .conteiner {
-  padding: 154px;
-  /* padding: 30px; */
+  padding: 142px;
+}
+
+#espaco{
+  padding: 15px;
 }
 </style>
