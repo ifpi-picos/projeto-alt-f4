0e9bbc9ad9ab008e0270919de90c5f9f43932ff0
Lucas mattos <lucasmattos251012@gmail.com>
2020-12-14 14:18:41 -0300

    noticias
    subpagina

---
+++ src/views/noticia.vue
@@ -0,0 +1,95 @@
+<template >
+  <div class="container">
+
+            <div >
+                <div>
+                    <h1>Meme Card</h1>
+
+                    <h4> Card game online sobre os memes classicos </h4>
+
+                    <div>
+                        <small><a href="" title="">21 July, 2019</a></small>
+                        <small><a href="" title="">by Cinthia Raquel</a></small>
+                        <small><a href="" title=""></a></small>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+            <div>
+                <div >
+                    <div >
+                      <a href=""><img src="img/svg/insta.svg"></a>
+                      <a href=""><img src="img/svg/twitter.svg"></a>
+                      <a href=""><img src="img/svg/github.svg"></a>
+                    </div>
+
+                    <p>
+                        In lobortis pharetra mattis. Morbi nec nibh iaculis, bibendum augue a, ultrices nulla. Nunc velit ante, lacinia id tincidunt eget, faucibus nec nisl. In mauris purus, bibendum et gravida dignissim, venenatis commodo lacus. Duis consectetur quis nisi nec
+                        accumsan. Pellentesque enim velit, ut tempor turpis. Mauris felis neque, egestas in lobortis et,iaculis at nunc ac, rhoncus sagittis ipsum.
+                    </p>
+
+                    <p>
+                        Vivamus non condimentum orci. Pellentesque venenatis nibh sit amet est vehicula lobortis. Cras eget aliquet eros. Nunc lectus elit, suscipit at nunc sed, finibus imperdiet ipsum. Maecenas dapibus neque sodales nulla finibus volutpat. Integer pulvinar
+                        massa vitae ultrices posuere. Proin ut tempor turpis. Mauris felis neque, egestas in lobortis et, sodales non ante. Ut vestibulum libero quis luctus tempus. Nullam eget dignissim massa. Vivamus id condimentum orci. Nunc ac sem
+                        urna. Aliquam et hendrerit nisl massa nunc.
+                    </p>
+                </div>
+
+                <div>  
+                    <b >    
+                        Maecenas non convallis quam, eu sodales justo. Pellentesque quis lectus elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.                      
+                    </b>
+
+                    <p>
+                        Vivamus non condimentum orci. Pellentesque venenatis nibh sit amet est vehicula lobortis. Cras eget aliquet eros. Nunc lectus elit, suscipit at nunc sed, finibus imperdiet ipsum. Maecenas dapibus neque sodales nulla finibus volutpat. Integer pulvinar
+                        massa vitae ultrices posuere. Proin ut tempor turpis. Mauris felis neque, egestas in lobortis et, sodales non ante. Ut vestibulum libero quis luctus tempus. Nullam eget dignissim massa. Vivamus id condimentum orci. Nunc ac sem
+                        urna. Aliquam et hendrerit nisl massa nunc.
+                    </p>
+
+
+                </div>
+
+            </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      noticia: {
+        title: "Avelar Chronicles",
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+  p span{
+    font-size: small;
+}
+
+p{
+    font-size: 20px;
+    text-align: start;
+    line-height: 1.5;
+    padding-bottom: 15px;
+}
+
+
+.logo-rodape h3 {
+    font-size: 28px;
+}
+
+b{
+    font-size: 25px;
+    padding: 20px;
+}
+
+
+</style>
