18f90e0524fc7f399b6bd6940c929a953fb4ae96
Lukas00-gif <lucassky08@gmail.com>
2020-12-18 16:08:31 -0300

    functions incomp

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,28 @@
-'use strict'
 const functions = require('firebase-functions')
 const nodemailer = require('nodemailer')
 const cors = require('cors')({ origin: true })
 
-let url =
-  'smtps://<SEU-EMAIL>%40gmail.com:' +
-  encodeURIComponent('<SUA-SENHA>') +
-  '@smtp.gmail.com:465'
-let transporter = nodemailer.createTransport(url)
+
+const smtpTransport = mailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: false, //SSL/TLS
+  auth: {
+      user: 'horusmaptracking@gmail.com',
+      pass: 'horus2020'
+  }
+})
 
 exports.enviarEmail = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
-    let remetente = '"Adson Rocha" <email@gmail.com>'
+    const remetente = '"Radical Dreamers" <horusmaptracking@gmail.com>'
 
-    let assunto = req.body['assunto']
-    let destinatarios = req.body['destinatarios'] // lista de e-mails destinatarios separados por ,
-    let corpo = req.body['corpo']
-    let corpoHtml = req.body['corpoHtml']
+    const assunto = req.body['assunto']
+    const destinatarios = req.body['destinatarios'] // lista de e-mails destinatarios separados por ,
+    const corpo = req.body['corpo']
+    const corpoHtml = req.body['corpoHtml']
 
-    let email = {
+    const email = {
       from: remetente,
       to: destinatarios,
       subject: assunto,
@@ -26,7 +30,7 @@ exports.enviarEmail = functions.https.onRequest((req, res) => {
       html: corpoHtml
     }
 
-    transporter.sendMail(email, (error, info) => {
+    smtpTransport.sendMail(email, (error, info) => {
       if (error) {
         return console.log(error)
       }
