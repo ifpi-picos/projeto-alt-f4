142f201d8905057936b07c484f1b1445d2838676
Jailson Jhonatas <mrjk044@gmail.com>
2020-12-23 18:36:11 -0300

    Noticia rota

--- src/router/index.js
+++ src/router/index.js
@@ -7,76 +7,73 @@ import Sidebar from '../template/Sidebar.vue'
 
 Vue.use(VueRouter)
 
-const routes = [{
-  path: '/',
-  name: 'Template',
-  component: Padrao,
-  redirect: '/home',
-  children: [{
-    path: '/home',
-    name: 'Home',
-    component: () => import('../views/Home.vue')
+const routes = [
+  {
+    path: '/',
+    name: 'Template',
+    component: Padrao,
+    redirect: '/home',
+    children: [
+      {
+        path: '/home',
+        name: 'Home',
+        component: () => import('../views/Home.vue')
+      },
+      {
+        path: '/games',
+        name: 'Games',
+        component: () => import('../views/Games.vue')
+      },
+      {
+        path: '/game/:id',
+        name: 'Game',
+        component: () => import('../views/Game.vue')
+      },
+      {
+        path: '/noticias',
+        name: 'Noticias',
+        component: () => import('../views/Noticias.vue')
+      },
+      {
+        path: '/noticia/:id',
+        name: 'Noticia',
+        component: () => import('../views/Noticia.vue')
+      },
+      {
+        path: '/sobre',
+        name: 'Sobre',
+        component: () => import('../views/Sobre.vue')
+      },
+      {
+        path: '/contato',
+        name: 'Contato',
+        component: () => import('../views/Contato.vue')
+      }
+    ]
   },
   {
-    path: '/games',
-    name: 'Games',
-    component: () =>
-      import('../views/Games.vue')
+    path: '/admin',
+    name: 'login',
+    component: () => import('../views/admin/Login.vue')
   },
   {
-    path: '/game/:id',
-    name: 'Game',
-    component: () =>
-      import('../views/Game.vue')
-  },
-  {
-    path: '/noticias',
-    name: 'Noticias',
-    component: () =>
-      import('../views/Noticias.vue')
-  },
-  {
-    path: '/noticia/:id',
-    name: 'Noticia',
-    component: () =>
-      import('../views/noticia.vue')
-  },
-  {
-    path: '/sobre',
-    name: 'Sobre',
-    component: () =>
-      import('../views/Sobre.vue')
-  },
-  {
-    path: '/contato',
-    name: 'Contato',
-    component: () =>
-      import('../views/Contato.vue')
+    path: '/admin',
+    name: 'Sidebar',
+    component: Sidebar,
+    children: [
+      {
+        path: '/home-admin',
+        name: 'home-admin',
+        component: () => import('../views/admin/HomeAdmin.vue')
+      },
+      {
+        path: '/cadastro-noticia',
+        name: 'Cadastro-noticia',
+        component: () => import('../views/admin/CadastroNoticia.vue')
+      }
+    ]
   }
-  ]
-},
-{
-  path: '/admin',
-  name: 'login',
-  component: () => import('../views/admin/Login.vue')
-},
-{
-  path: '/admin',
-  name: 'Sidebar',
-  component: Sidebar,
-  children: [{
-    path: '/home-admin',
-    name: 'home-admin',
-    component: () =>
-      import('../views/admin/HomeAdmin.vue')
-  },
-  {
-    path: '/cadastro-noticia',
-    name: 'Cadastro-noticia',
-    component: () =>
-      import('../views/admin/CadastroNoticia.vue')
-  }]
-}]
+]
 
 const router = new VueRouter({
   mode: 'history',
