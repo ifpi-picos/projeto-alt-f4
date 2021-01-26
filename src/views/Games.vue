d4778432d9c991e58d1bc9c463a82983f1b926f2
Janderson <51001932+droid-janderson@users.noreply.github.com>
2021-01-26 19:34:32 -0300

    Ajustes pagina de noticias, pagina de game de forma dinamica

--- src/views/Games.vue
+++ src/views/Games.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="container-fluid">
     <section class="row justify-content-center">
+      <div v-if="isLoading">
+        <b-button variant="primary" disabled>
+          <b-spinner small type="grow"></b-spinner>
+          Loading...
+        </b-button>
+      </div>
+
       <div v-for="card in cards" :key="card.id" class="p-0 col-md-3 col-sm-6">
         <b-card
-          img-src="img/640x790/AVELAR.png"
-          img-alt="Image"
+          v-bind:img-src="card.data().cardImg"
+          v-bind:img-alt="card.data().titulo"
           overlay
           class="p-0 my-3 mx-5 border-light"
           @click="showDetails(card.id)"
@@ -19,30 +26,62 @@
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       cards: [],
-    };
-  },
-  methods: {
-    showDetails(id) {
-      this.$router.push({ name: "Game", params: { id: id } });
-    },
-
-    consultar () {
-      const cards = this.$firebase.firestore().collection('noticias')
-
-      cards.get()
-        .then((querySnapshot) => {
-          querySnapshot.forEach((doc) => {
-            var card = doc.data()
-            console.log(card)
-          });
-        })
-        .catch((error) => {
-          console.error(error);
-        })
+      isLoading: true
     }
   },
-};
+
+  mounted () {
+    this.fetchCards()
+  },
+
+  methods: {
+    showDetails (id) {
+      this.$router.push({ name: 'Game', params: { id } })
+    },
+
+    setLoading (state) {
+      if (typeof state !== 'boolean') {
+        return
+      }
+
+      this.isLoading = state
+    },
+
+    async fetchNoticias () {
+      return await this.$firebase
+        .firestore()
+        .collection('noticias')
+        .where('selecao', 'array-contains', 'game')
+        .get()
+    },
+
+    async fetchCards () {
+      try {
+        const cards = await this.fetchNoticias()
+
+        if (cards.length === 0) {
+          return
+        }
+
+        cards.forEach(card => {
+          this.cards.push(card)
+        })
+
+        this.setLoading(false)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+}
 </script>
+
+<style scoped>
+.container {
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+</style>
--- src/views/Noticias.vue
+++ src/views/Noticias.vue
@@ -2,20 +2,24 @@
   <div class="container">
     <section class="p-0 row justify-content-center">
       <div v-if="isLoading">
-        Carregando...
+        <b-button variant="primary" disabled>
+          <b-spinner small type="grow"></b-spinner>
+          Loading...
+        </b-button>
       </div>
 
       <div v-for="card in cards" :key="card.id" class="col-md-4 col-sm-12">
         <b-card
-          v-bind:img-src="card.cardImg"
-          v-bind:img-alt="card.titulo"
+          v-bind:img-src="card.data().cardImg"
+          v-bind:img-alt="card.data().titulo"
           img-top
           tag="article"
           style="max-width: 20rem; cursor: pointer"
           class="mb-4"
           @click="showDetails(card.id)"
         >
-          <h4>{{card.titulo}}</h4>
+          <h4>{{ card.data().titulo }}</h4>
+          <p>{{ card.data().autor }}</p>
         </b-card>
       </div>
     </section>
@@ -24,32 +28,36 @@
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       cards: [],
       isLoading: true
     }
   },
 
-  mounted() {
+  mounted () {
     this.fetchCards()
   },
 
   methods: {
-    showDetails(id) {
-      this.$router.push({ name: "Noticia", params: { id } });
+    showDetails (id) {
+      this.$router.push({ name: 'Noticia', params: { id } })
     },
 
-    setLoading(state) {
-      if (typeof(state) !== 'boolean') {
+    setLoading (state) {
+      if (typeof state !== 'boolean') {
         return
       }
 
       this.isLoading = state
     },
 
-    async fetchNoticias() {
-      return await this.$firebase.firestore().collection('noticias').get()
+    async fetchNoticias () {
+      return await this.$firebase
+        .firestore()
+        .collection('noticias')
+        .where("selecao", "array-contains", "noticia")
+        .get()
     },
 
     async fetchCards () {
@@ -60,27 +68,30 @@ export default {
           return
         }
 
-        cards.forEach((card) => this.cards.push(card.data()))
+        cards.forEach(card => {
+          this.cards.push(card)
+        })
 
-        this.setLoading(false)        
-      } catch (err) { 
+        this.setLoading(false)
+      } catch (err) {
         console.log(err)
       }
     }
-  },
-};
+  }
+}
 </script>
 <style scoped>
 .container {
-    margin-top: 50px;
-    margin-bottom: 50px;
-  }
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
 
-  p span{
-    font-size: 12px;
-  }
+p span {
+  font-size: 12px;
+}
 
-  b-card-text, p{
-    font-size: 20px;
-  }
+b-card-text,
+p {
+  font-size: 20px;
+}
 </style>
